b9b97232b075571f3ff57bfb64190fac57beefac
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2024-09-28 02:51:55 -0700

    P

--- CINEPOLIS_JDBC/src/main/java/PruebasMetodos/Pruebas.java
+++ CINEPOLIS_JDBC/src/main/java/PruebasMetodos/Pruebas.java
@@ -4,6 +4,22 @@
  */
 package PruebasMetodos;
 
+import Negocio.DTOs.ClienteDTO;
+import Persistencia.DAOs.ClienteDAO;
+import Persistencia.DAOs.ConexionBD;
+import Persistencia.DAOs.IClienteDAO;
+import Persistencia.DAOs.IConexionBD;
+import Persistencia.DAOs.IPeliculaDAO;
+import Persistencia.DAOs.PeliculaDAO;
+import Persistencia.Entidades.Ciudad;
+import Persistencia.Entidades.Cliente;
+import excepciones.cinepolisException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author rramirez
@@ -13,8 +29,14 @@ public class Pruebas {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws cinepolisException {
+        IConexionBD conexionBD = new ConexionBD();
+        IPeliculaDAO peliculaDAO = new PeliculaDAO(conexionBD);
+        
+        System.out.println(        peliculaDAO.obtenerTodasLasPeliculas()
+);        
     }
-    
+
 }
+
+
